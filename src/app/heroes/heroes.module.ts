a194777950ec0bcc6c0768d61a671907faa6b382
Cristian Garib <cristian@cristiangarib.com>
2024-05-30 09:05:38 -0400

    primer commit

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroComponent } from './hero/hero.component';
